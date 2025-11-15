3226610f98fff79be0ab660a80bfbf9cede55eed
asmacorporate-rgb <asmacorporate1@gmail.com>
2025-11-15 20:02:36 +0600

    Fixed Errors and ran tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,13 @@
 // @ts-check
+
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   use: {
-    headless: false,
+    headless: false,         // run in visible browser
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    baseURL: "https://www.saucedemo.com"
+    baseURL: "https://www.saucedemo.com",
+    slowMo: 10000              // slows down actions by 500ms
   }
 });
